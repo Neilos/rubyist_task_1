75c3550e080c20803800110235f1b9b6a621c3b5
Neil Atkinson <nosniktatkinson@hotmail.co.uk>
2013-06-14 20:45:42 +0100

    recursive inject implemented

--- lib/array.rb
+++ lib/array.rb
@@ -1,24 +1,23 @@
 class Array
 
-  def inject(initial=nil)
-    result = initial
-    self.each do |element|
-      if result; result = yield(result, element)
-      else; result = element; end
-    end
-    result
+  def inject(initial=nil, &block) 
+    recursive_inject(initial || self[0], initial ? 0 : 1, &block)
   end
 
   def reduce(operator)
-      recursive_reduce(operator, 0)
+    recursive_reduce(operator, 0)
   end
 
 private
   
-  def recursive_reduce(operator, i)
-    return nil if self.length == 0
-    return self[i] if i == self.length - 1
-    recursive_reduce(operator, i + 1).send(operator, self[i])
+  def recursive_inject(result, index, &block)
+    return result if index == count 
+    recursive_inject(block.call(result, self[index]), index + 1, &block)
+  end
+
+  def recursive_reduce(operator, index)
+    return self[index] if index >= count - 1
+    recursive_reduce(operator, index + 1).send(operator, self[index])
   end
 
 end
