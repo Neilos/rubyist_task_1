3ae3a5eacee14546e095367e35d39e16d91de97e
Neil Atkinson <nosniktatkinson@hotmail.co.uk>
2013-06-14 18:06:13 +0100

    recursive 'reduce' implemented

--- lib/array.rb
+++ lib/array.rb
@@ -9,13 +9,16 @@ class Array
     result
   end
 
-  def reduce(operation)
-    result = nil
-    self.each do |element|
-      if result; result = result.send(operation, element)
-      else; result = element; end
-    end
-    result
+  def reduce(operator)
+      recursive_reduce(operator, 0)
+  end
+
+private
+  
+  def recursive_reduce(operator, i)
+    return nil if self.length == 0
+    return self[i] if i == self.length - 1
+    recursive_reduce(operator, i + 1).send(operator, self[i])
   end
 
 end
